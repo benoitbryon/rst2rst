2ab421eb3564c35a12722a7999558f43e561f760
Benoit Bryon <benoit@marmelune.net>
2012-07-06 09:50:43 +0200

    Refs #12 - Implemented PEP 396, i.e. rst2rst.__version__

--- rst2rst/__init__.py
+++ rst2rst/__init__.py
@@ -1,3 +1,9 @@
 """rst2rst."""
+import os
+
 from writer import Writer
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+__version__ = open(os.path.join(current_dir, 'version.txt')).read().strip()
 
