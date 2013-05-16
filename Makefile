68bae180d286d682d2c23a7e7c7639a41e399cda
Benoît Bryon <benoit@marmelune.net>
2013-05-16 10:13:05 +0200

    Improved packaging and development environment (buildout and nose configuration, Makefile).

--- Makefile
+++ Makefile
@@ -1,18 +1,57 @@
-develop:
-	if [ ! -f lib/buildout/bootstrap.py ]; then \
-	    mkdir -p lib/buildout; \
-	    wget http://svn.zope.org/*checkout*/zc.buildout/tags/1.5.2/bootstrap/bootstrap.py?content-type=text%2Fplain -O lib/buildout/bootstrap.py; \
-	    python lib/buildout/bootstrap.py --distribute; \
-	fi
-	bin/buildout -N
+# Makefile for development.
+# See INSTALL and docs/contribute/index.txt for details.
+SHELL = /bin/bash
+ROOT_DIR = $(shell pwd)
+BIN_DIR = $(ROOT_DIR)/bin
+DATA_DIR = $(ROOT_DIR)/var
+WGET = wget
+PYTHON = $(shell which python)
+PROJECT = $(shell $(PYTHON) -c "import setup; print setup.NAME")
+BUILDOUT_CFG = $(ROOT_DIR)/etc/buildout.cfg
+BUILDOUT_DIR = $(ROOT_DIR)/lib/buildout
+BUILDOUT_VERSION = 1.7.1
+BUILDOUT_BOOTSTRAP_URL = https://raw.github.com/buildout/buildout/$(BUILDOUT_VERSION)/bootstrap/bootstrap.py
+BUILDOUT_BOOTSTRAP = $(BUILDOUT_DIR)/bootstrap.py
+BUILDOUT_BOOTSTRAP_ARGS = -c $(BUILDOUT_CFG) --version=$(BUILDOUT_VERSION) --distribute buildout:directory=$(ROOT_DIR)
+BUILDOUT = $(BIN_DIR)/buildout
+BUILDOUT_ARGS = -N -c $(BUILDOUT_CFG) buildout:directory=$(ROOT_DIR)
+NOSE = $(BIN_DIR)/nosetests
 
-update: develop
 
-uninstall:
-	rm -r bin/ lib/
+configure:
+	# Configuration is stored in etc/ folder. Not generated yet.
 
-tests:
-	bin/nosetests -v --rednose --with-doctest --with-coverage --cover-erase --nocapture --cover-package=rst2rst rst2rst/
+
+develop: buildout
+
+
+buildout:
+	if [ ! -d $(BUILDOUT_DIR) ]; then mkdir -p $(BUILDOUT_DIR); fi
+	if [ ! -f $(BUILDOUT_BOOTSTRAP) ]; then wget -O $(BUILDOUT_BOOTSTRAP) $(BUILDOUT_BOOTSTRAP_URL); fi
+	if [ ! -x $(BUILDOUT) ]; then $(PYTHON) $(BUILDOUT_BOOTSTRAP) $(BUILDOUT_BOOTSTRAP_ARGS); fi
+	$(BUILDOUT) $(BUILDOUT_ARGS)
+
+
+clean:
+	find $(ROOT_DIR)/ -name "*.pyc" -delete
+	find $(ROOT_DIR)/ -name ".noseids" -delete
+
+
+distclean: clean
+	rm -rf $(ROOT_DIR)/*.egg-info
+
+
+maintainer-clean: distclean
+	rm -rf $(BIN_DIR)/
+	rm -rf $(ROOT_DIR)/lib/
+
+
+test: test-app
+
+
+test-app:
+	$(NOSE) --config=etc/nose.cfg --with-doctest --with-coverage --cover-erase --cover-package=rst2rst rst2rst
+
 
 release:
-	bin/fullrelease
+	$(BIN_DIR)/fullrelease
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 """Python packaging."""
 import os
 from setuptools import setup
@@ -8,38 +8,40 @@ def read_relative_file(filename):
     """Returns contents of the given file, which path is supposed relative
     to this module."""
     with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-        return f.read()
+        return f.read().strip()
 
 
 NAME = 'rst2rst'
 README = read_relative_file('README.rst')
 VERSION = read_relative_file(os.path.join(NAME, 'version.txt')).strip()
+PACKAGES = [NAME]
+REQUIRES = ['setuptools', 'docutils']
+ENTRY_POINTS = {
+    'console_scripts': [
+        'rst2rst = rst2rst.scripts.rst2rst:main',
+    ]
+}
 
-
-setup(name=NAME,
-      version=VERSION,
-      description='Transform reStructuredText documents. Standardize RST syntax',
-      long_description=README,
-      classifiers=['Development Status :: 3 - Alpha',
-                   'License :: OSI Approved :: BSD License',
-                   'Programming Language :: Python :: 2.7',
-                   'Topic :: Documentation',
-                   'Topic :: Software Development :: Documentation',
-                   'Topic :: Software Development :: Quality Assurance',
-                   'Topic :: Text Processing',
-                   ],
-      keywords='rst writer reStructuredText',
-      author='Benoit Bryon',
-      author_email='benoit@marmelune.net',
-      url='https://github.com/benoitbryon/%s' % NAME,
-      license='BSD',
-      packages=[NAME],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools', 'docutils'],
-      entry_points={
-          'console_scripts': [
-              'rst2rst = rst2rst.scripts.rst2rst:main',
-          ]
-      },
-      )
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(name=NAME,
+          version=VERSION,
+          description='Transform reStructuredText documents. Standardize RST syntax',
+          long_description=README,
+          classifiers=['Development Status :: 3 - Alpha',
+                       'License :: OSI Approved :: BSD License',
+                       'Programming Language :: Python :: 2.7',
+                       'Topic :: Documentation',
+                       'Topic :: Software Development :: Documentation',
+                       'Topic :: Software Development :: Quality Assurance',
+                       'Topic :: Text Processing',
+                       ],
+          keywords='rst writer reStructuredText',
+          author='Benoît Bryon',
+          author_email='benoit@marmelune.net',
+          url='https://github.com/benoitbryon/%s' % NAME,
+          license='BSD',
+          packages=[NAME],
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=REQUIRES,
+          entry_points=ENTRY_POINTS)
