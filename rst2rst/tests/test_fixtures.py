5548e89f86210e8fc0f30da4a9b06d20a8cbeb5e
Benoît Bryon <benoit@marmelune.net>
2013-05-22 14:50:02 +0200

    Refs #19 - Simplified rst2rst.tests.test_fixtures: statically created test_* methods, fixed compatibility with nose and unittest2 (Python 2.7).

--- rst2rst/tests/test_fixtures.py
+++ rst2rst/tests/test_fixtures.py
@@ -8,108 +8,99 @@ import unittest
 from docutils.core import publish_string
 
 
-FILE_DIR = os.path.normpath(
-    os.path.abspath(
-        os.path.dirname(__file__)))
-FIXTURES_DIR = os.path.join(FILE_DIR, 'fixtures')
+#: Absolute path to ``rst2rst.tests`` module.
+TESTS_DIR = os.path.normpath(os.path.abspath(os.path.dirname(__file__)))
+
+#: Absolute path to :file:`fixtures` directory within ``rst2rst.tests`` module.
+FIXTURE_DIR = os.path.join(TESTS_DIR, 'fixtures')
 
 
-class TestMeta(type):
-    """ Unittest is a pain in the ass: it calls dir() on the *class*,
-    and then goes and gets each found method (on the class again) to
-    check that they're actually callables. But the calling itself is
-    performed on the instance.
+def fixture_names(fixture_dir=FIXTURE_DIR):
+    """Return list of fixture names.
 
-    And of course fail() needs an instance, so the actual fixture
-    runner has to be on the instance. And thus __getattr__ must be
-    implemented on both the class (via metaclass) and the instance.
+    By default, ``fixtures_dir`` is
+    :py:attr:`rst2rst.tests.test_fixtures.FIXTURE_DIR`.
+
+    Fixture names are computed from filenames in ``fixture_dir``.
+
     """
-    def __dir__(cls):
-        return sorted(set(
-            dir(type(cls))
-            + list(cls.__dict__)
-            + cls._fixture_methods()
-        ))
+    return sorted([
+        re.sub(r'^(.+?)-input\.txt$', r'\1', f)
+        for f in os.listdir(fixture_dir)
+        if f.endswith('-input.txt')
+    ])
 
-    def __getattr__(cls, attrname):
-        """ Return a dummy callable if ``attrname`` is
-        ``test_$fixture`` so unittest shuts up
-        """
-        input_path, output_path = cls._fixture_paths(attrname)
 
-        return lambda: None
+def fixture_method(name):
+    """Return function that checks fixture by ``name`` in a TestCase."""
+    return lambda obj: obj.run_fixture(*(obj.fixture_paths(name)))
+
+
+def fixtures_class(cls, bases, attrs):
+    """Dynamically add a test method for each available fixture."""
+    docstring = """"rst2rst" writer publish "{name}" fixture as expected."""
+    for fixture_name in fixture_names():
+        method_name = 'test_{name}'.format(name=fixture_name)
+        attrs[method_name] = fixture_method(fixture_name)
+        attrs[method_name].__name__ = method_name  # Required for nose.
+        attrs[method_name].__doc__ = docstring.format(name=fixture_name)
+    return type(cls, bases, attrs)
 
 
 class WriterTestCase(unittest.TestCase):
     """Test suite for the rst2rst.writer.Writer class."""
-    __metaclass__ = TestMeta
+    # Specific metaclass, in order to have one test_* method for each available
+    # fixture. Benefits:
+    #
+    # * Better overview/progress notifications ;
+    # * Failure of a fixture does not preclude trying out other fixtures.
+    __metaclass__ = fixtures_class
 
-    @classmethod
-    def _fixture_methods(cls):
-        """ Lists the names of all fixtures in the ./fixtures
-        directory (*-input.txt files with the -input.txt part
-        stripped), returns them as test_-prefixed names so unittest
-        believes they're test methods
+    def get_fixture_dir(self):
+        """Return :py:attr:`rst2rst.tests.test_fixtures.FIXTURE_DIR`."""
+        return FIXTURE_DIR
+
+    def fixture_paths(self, name):
+        """Return ``(input, output)`` filenames for fixture ``name``.
+
+        Also asserts both files exist.
+
         """
-        return [
-            re.sub(r'^(.+?)-input\.txt$', r'test_\1', f)
-            for f in os.listdir(FIXTURES_DIR)
-            if f.endswith('-input.txt')
-        ]
+        paths = []
+        for suffix in ['input', 'output']:
+            filename = '%s-%s.txt' % (name, suffix)
+            path = os.path.join(self.get_fixture_dir(), filename)
+            self.assertTrue(
+                os.path.exists(path),
+                "Missing file '%s' for fixture '%s'" % (filename, name))
+            paths.append(path)
+        return paths
 
-    @classmethod
-    def _fixture_paths(cls, attrname):
-        """ For a fixture named $fixture, gets the corresponding
-        -input and -output files paths and asserts both exist. The
-        attribute fetched should be test_$fixture
+    def run_fixture(self, source, reference):
+        """Assert rst2rst writer converts ``source`` to ``reference``.
+
+        Both ``source`` and ``reference`` are paths to files.
+
         """
-        if not attrname.startswith('test_'):
-            raise AttributeError("'%s' object has no attribute '%s'" % (
-                cls.__name__, attrname))
-
-        fixture = attrname.replace('test_', '', 1)
-
-        fixture_input = '%s-input.txt' % fixture
-        input_path = os.path.join(FIXTURES_DIR, fixture_input)
-        if not os.path.exists(input_path):
-            raise AttributeError("Missing input file '%s' for fixture %s" % (
-                fixture_input, fixture))
-
-        fixture_result = '%s-output.txt' % fixture
-        result_path = os.path.join(FIXTURES_DIR, fixture_result)
-        if not os.path.exists(result_path):
-            raise AttributeError("Missing result file '%s' for fixture %s" % (
-                fixture_result, fixture))
-
-        return input_path, result_path
-
-    def __getattr__(self, attrname):
-        input_path, output_path = self._fixture_paths(attrname)
-
-        return lambda: self.run_fixture(input_path, output_path)
-
-    def run_fixture(self, fixture_input, fixture_expectation):
-        """ Runs the rst2rst writer on ``fixture_input`` and asserts
-        the result matches ``fixture_expectation``
-        """
-        with open(fixture_input) as input_file:
-            input_str = input_file.read()
-        output = publish_string(source=input_str, writer_name='rst2rst')
-
-        with open(fixture_expectation) as expectation_file:
-            expected = expectation_file.read()
-
-        if output != expected:
-            output_lines = output.replace('\n', '\\n\n').splitlines(True)
+        input_str = open(source).read()
+        real = publish_string(source=input_str, writer_name='rst2rst')
+        expected = open(reference).read()
+        if real != expected:
+            # Report error with a diff.
+            real_lines = real.replace('\n', '\\n\n').splitlines(True)
             expected_lines = expected.replace('\n', '\\n\n').splitlines(True)
-
-            diff = ''.join(unified_diff(expected_lines, output_lines))
+            diff = ''.join(unified_diff(expected_lines, real_lines))
             msg = "Content generated from %s differs from content at %s" \
-                  "\nDiff:\n%s" % (os.path.basename(fixture_input),
-                                   os.path.basename(fixture_expectation),
+                  "\nDiff:\n%s" % (os.path.basename(source),
+                                   os.path.basename(reference),
                                    diff)
             self.fail(msg)
 
-    def test_repeatability(self):
-        """Make sure that a converted document keeps the same if converted
-        again."""
+
+class FixtureTestTestCase(unittest.TestCase):
+    def test_fixtures_methods(self):
+        """WriterTestCase has test method for every fixture."""
+        test_class = WriterTestCase
+        for name in fixture_names():
+            method_name = 'test_{name}'.format(name=name)
+            self.assertTrue(callable(getattr(test_class, method_name)))
